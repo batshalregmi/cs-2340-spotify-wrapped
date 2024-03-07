7d13cb8d5c8da727024b7ef128597f288f0cd7dd
mariobob <35691890+Mariobob@users.noreply.github.com>
2024-03-07 12:50:18 -0500

    change behavior to put song instead of data

--- app/src/main/java/com/group3/spotifywrapped/MainActivity.java
+++ app/src/main/java/com/group3/spotifywrapped/MainActivity.java
@@ -1,10 +1,15 @@
 package com.group3.spotifywrapped;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +21,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -34,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
     private final OkHttpClient mOkHttpClient = new OkHttpClient();
     private String mAccessToken, mAccessCode;
     private Call mCall;
+    public JSONObject resp;
 
     private TextView tokenTextView, codeTextView, profileTextView;
 
@@ -41,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
 
         // Initialize the views
 //        TODO: ADD THE ConstraintLayout FROM THE TUTORIAL
@@ -52,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
         Button tokenBtn = (Button) findViewById(R.id.token_btn);
         Button codeBtn = (Button) findViewById(R.id.code_btn);
         Button profileBtn = (Button) findViewById(R.id.profile_btn);
+        ImageView profileImageView = (ImageView) findViewById(R.id.mainMenuImageView);
+
 
         // Set the click listeners for the buttons
 
@@ -65,7 +79,12 @@ public class MainActivity extends AppCompatActivity {
 
         profileBtn.setOnClickListener((v) -> {
             onGetUserProfileClicked();
+            profileImageView.setImageBitmap(getBitmapFromURL("https://i.scdn.co/image/ab67616d0000b273ab9d1ae18b640b7b0ce390d4"));
+
+            //set the image view to the profile picture
+//            profileImageView.setImageResource(resp);
         });
+
 
     }
 
@@ -112,6 +131,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            Log.e("src",src);
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap","returned");
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+            return null;
+        }
+    }
+
     /**
      * Get user profile
      * This method will get the user profile using the token
@@ -124,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Create a request to get the user profile
         final Request request = new Request.Builder()
-                .url("https://api.spotify.com/v1/me")
+                .url("https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=2")
                 .addHeader("Authorization", "Bearer " + mAccessToken)
                 .build();
 
@@ -141,9 +178,15 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
+                ImageView profileImageView = (ImageView) findViewById(R.id.mainMenuImageView);
                 try {
                     final JSONObject jsonObject = new JSONObject(response.body().string());
-                    setTextAsync(jsonObject.toString(3), profileTextView);
+//                    setTextAsync(jsonObject.toString(3), profileTextView);
+                    Log.d("JSON", "Got response: " + jsonObject.toString(3));
+                    resp = jsonObject;
+                    String urlProfilePicture = jsonObject.getJSONArray("items").getJSONObject(0).getJSONObject("album").getJSONArray("images").getJSONObject(0).getString("url");
+                    //set the image view to the profile picture
+
                 } catch (JSONException e) {
                     Log.d("JSON", "Failed to parse data: " + e);
                     Toast.makeText(MainActivity.this, "Failed to parse data, watch Logcat for more details",
@@ -173,7 +216,7 @@ public class MainActivity extends AppCompatActivity {
     private AuthorizationRequest getAuthenticationRequest(AuthorizationResponse.Type type) {
         return new AuthorizationRequest.Builder(CLIENT_ID, type, getRedirectUri().toString())
                 .setShowDialog(false)
-                .setScopes(new String[] { "user-read-email" }) // <--- Change the scope of your requested token here
+                .setScopes(new String[] { "user-read-email", "user-top-read"}) // <--- Change the scope of your requested token here
                 .setCampaign("your-campaign-token")
                 .build();
     }

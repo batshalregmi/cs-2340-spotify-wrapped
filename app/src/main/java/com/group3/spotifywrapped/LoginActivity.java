61cfd06d0b0539a2f1b52e6a3fe5ef6d9ee8e4eb
vivek3210 <36582094+vivek3210@users.noreply.github.com>
2024-03-09 17:31:11 -0500

    added login page w/ new image

---
+++ app/src/main/java/com/group3/spotifywrapped/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.group3.spotifywrapped;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity {
+    EditText username;
+    EditText password;
+    Button loginButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}

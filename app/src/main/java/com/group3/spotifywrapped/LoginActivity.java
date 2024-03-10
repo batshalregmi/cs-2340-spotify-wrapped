6594a282d9b82e30e790b19f01cba170e2d85549
Batshal Regmi <35691890+Mariobob@users.noreply.github.com>
2024-03-09 23:10:11 -0500

    undo no easy way to get around github branch lock

--- app/src/main/java/com/group3/spotifywrapped/LoginActivity.java
+++ app/src/main/java/com/group3/spotifywrapped/LoginActivity.java
@@ -10,8 +10,8 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     EditText username;
-        EditText password;
-                    Button loginButton;
+    EditText password;
+    Button loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +19,7 @@ public class LoginActivity extends AppCompatActivity {
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
         loginButton = findViewById(R.id.loginButton);
-  loginButton.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {

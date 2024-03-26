05df32478a76f69e025e9b2c867046f497bc7b8b
tdoiron0 <trentwdoiron@gmail.com>
2024-03-26 15:28:45 -0400

    Working on making database work with summary view

--- app/src/main/java/com/group3/spotifywrapped/LoginActivity.java
+++ app/src/main/java/com/group3/spotifywrapped/LoginActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
 
@@ -30,10 +31,6 @@ public class LoginActivity extends AppCompatActivity {
 
     private String mAccessToken;
 
-    EditText username;
-    EditText password;
-    Button loginButton;
-
     public AppDatabase db;
     public static UserDao userDao;
     public static User mUser;
@@ -44,13 +41,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
-        loginButton = findViewById(R.id.loginButton);
 
-        db = Room.databaseBuilder(getApplicationContext(),
-                AppDatabase.class, "local-database").build();
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "local-database").build();
         userDao = db.userDao();
+
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Button loginButton = findViewById(R.id.loginButton);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             String dbUsername;
@@ -68,26 +65,15 @@ public class LoginActivity extends AppCompatActivity {
                 });
                 thread.start();
 
-                // TODO: This is kind of buggy, will fix tonight
-                // Only works with emails with the password "password"
-                if (password.getText().toString().equals(dbPassword)) {
-                    password = (EditText) findViewById(R.id.password);
-                    loginButton = findViewById(R.id.loginButton);
-                    loginButton.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            if (username.getText().toString().equals(dbUsername) && password.getText().toString().equals(dbPassword)) {
-                                getUser(username.getText().toString());
-                                Intent i = new Intent(LoginActivity.this, TopSongsSummaryActivity.class);
-                                startActivity(i);
-                            } else {
-                                Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
-                            }
-                            System.out.println(password.getText().toString());
-                            System.out.println(dbPassword);
-                        }
-                    });
+                if (username.getText().toString().equals(dbUsername) && password.getText().toString().equals(dbPassword)) {
+                    getUser(username.getText().toString());
+                    Intent i = new Intent(LoginActivity.this, TopSongsSummaryActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
+                System.out.println(password.getText().toString());
+                System.out.println(dbPassword);
             }
         });
     }

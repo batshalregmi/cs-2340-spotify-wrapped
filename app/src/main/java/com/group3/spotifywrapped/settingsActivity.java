ddc2a4d2a3ba6d078d7b37920f250c48fd55df87
vivek3210 <36582094+vivek3210@users.noreply.github.com>
2024-04-02 13:46:01 -0400

    popup page

--- app/src/main/java/com/group3/spotifywrapped/settingsActivity.java
+++ app/src/main/java/com/group3/spotifywrapped/settingsActivity.java
@@ -1,6 +1,54 @@
 package com.group3.spotifywrapped;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
-import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class settingsActivity extends Activity {
+import com.group3.spotifywrapped.utils.SpotifyApiHelper;
+import com.spotify.sdk.android.auth.AuthorizationClient;
+import com.spotify.sdk.android.auth.AuthorizationRequest;
+import com.spotify.sdk.android.auth.AuthorizationResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import database.AppDatabase;
+import database.User;
+import database.UserDao;
+
+public class settingsActivity extends AppCompatActivity {
+    Dialog myDialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        myDialog = new Dialog(this);
+    }
+    public void ShowPopup(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+    }
 }

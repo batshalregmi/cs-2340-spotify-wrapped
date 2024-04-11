92f4c0dc0cc2453ec7d400463afa1974de112d00
patelshubh1022 <patelshubh1022@gmail.com>
2024-04-10 22:21:58 -0400

    I am the scrum master bruh

--- app/src/main/java/com/group3/spotifywrapped/CoreAppViews/SettingsActivity.java
+++ app/src/main/java/com/group3/spotifywrapped/CoreAppViews/SettingsActivity.java
@@ -1,12 +1,15 @@
 package com.group3.spotifywrapped.CoreAppViews;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,10 +17,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
 import com.group3.spotifywrapped.R;
 import com.group3.spotifywrapped.database.FirebaseHelper;
 import com.group3.spotifywrapped.utils.SpotifyApiHelper;
@@ -29,12 +29,18 @@ import org.json.JSONObject;
 import java.io.IOException;
 
 public class SettingsActivity extends AppCompatActivity {
+    Dialog passwordDialog;
+    Dialog emailDialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        emailDialog = new Dialog(this);
+        passwordDialog = new Dialog(this);
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
@@ -117,5 +123,37 @@ public class SettingsActivity extends AppCompatActivity {
             builder.create();
 
         });
+
     }
+    public void ShowPopupEmail(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        emailDialog.setContentView(R.layout.email_custompop);
+        txtclose =(TextView) emailDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                emailDialog.dismiss();
+            }
+        });
+        emailDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        emailDialog.show();
+    }
+    public void ShowPopupPassword(View v) {
+        TextView txtclose;
+        Button btnFollow;
+        passwordDialog.setContentView(R.layout.password_custompopup);
+        txtclose =(TextView) passwordDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                passwordDialog.dismiss();
+            }
+        });
+        passwordDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        passwordDialog.show();
+    }
+
 }

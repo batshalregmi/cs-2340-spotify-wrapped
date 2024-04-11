dccc43ea5fd54a3be0214df173b7979001fd1ae5
tdoiron0 <trentwdoiron@gmail.com>
2024-04-11 18:34:12 -0400

    ...

--- app/src/main/java/com/group3/spotifywrapped/database/FirebaseHelper.java
+++ app/src/main/java/com/group3/spotifywrapped/database/FirebaseHelper.java
@@ -1,6 +1,7 @@
 package com.group3.spotifywrapped.database;
 
 import android.util.Log;
+import android.util.Pair;
 
 import androidx.annotation.NonNull;
 
@@ -11,6 +12,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.group3.spotifywrapped.summary.SummaryActivity;
+import com.group3.spotifywrapped.summary.SummarySelectorActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -70,41 +73,51 @@ public class FirebaseHelper {
 
     public static DatabaseReference addSummaryEntry(SummaryEntry newEntry, DatabaseReference userRef) {
         DatabaseReference result = userRef.child("entries").push();
-        result.setValue(newEntry);
+        result.child("dateCreated").setValue(newEntry.dateCreated.toString());
+        for (Artist it : newEntry.getArtists()) {
+            addArtist(it, result);
+        }
+        for (Track it : newEntry.getTracks()) {
+            addTrack(it, result);
+        }
+
         return result;
     }
 
     public static DatabaseReference addArtist(Artist artist, DatabaseReference entryRef) {
         DatabaseReference result = entryRef.child("artists").push();
-        result.setValue(artist);
+        result.child("spotifyId").setValue(artist.spotifyId);
+        result.child("name").setValue(artist.name);
+        result.child("imageUrl").setValue(artist.getImageUrl());
         return result;
     }
 
     public static DatabaseReference addTrack(Track track, DatabaseReference entryRef) {
         DatabaseReference result = entryRef.child("tracks").push();
-        result.setValue(track);
+        result.child("spotifyId").setValue(track.spotifyId);
+        result.child("name").setValue(track.name);
+        result.child("imageUrl").setValue(track.getImageUrl());
         return result;
     }
 
-    public static List<SummaryEntry> getEntryFromUser(DatabaseReference userRef) {
-        List<SummaryEntry> result = new ArrayList<>();
+    public static void getEntriesFromUser(DatabaseReference userRef, List<Pair<DatabaseReference, SummaryEntry>> result) {
         userRef.child("entries").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d(TAG, "Updating entry list");
                 result.clear();
                 for (DataSnapshot it : snapshot.getChildren()) {
-                    result.add(it.getValue(SummaryEntry.class));
+                    result.add(new Pair<DatabaseReference, SummaryEntry>(it.getRef(), new SummaryEntry(it.child("dateCreated").getValue(String.class))));
                 }
+                SummarySelectorActivity.foundEntries.set(true);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Log.w(TAG, "Failed to read entries", error.toException());
+                Log.w(TAG, "Failed to load summary entry list", error.toException());
             }
         });
-        return result;
     }
-
 
     public static List<SpotifyItem> getArtistsFromEntry(DatabaseReference entryRef) {
         List<SpotifyItem> result = new ArrayList<>();
@@ -113,9 +126,14 @@ public class FirebaseHelper {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 result.clear();
                 for (DataSnapshot it : snapshot.getChildren()) {
-                    result.add(it.getValue(Artist.class));
+                    Artist newArtist = new Artist(
+                            it.child("spotifyId").getValue(String.class),
+                            it.child("name").getValue(String.class),
+                            it.child("imageUrl").getValue(String.class)
+                    );
+                    result.add(newArtist);
                 }
-                Log.d(TAG, "artists found: " + result.toString());
+                SummaryActivity.foundArtists.set(true);
             }
 
             @Override
@@ -133,8 +151,14 @@ public class FirebaseHelper {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 result.clear();
                 for (DataSnapshot it : snapshot.getChildren()) {
-                    result.add(it.getValue(Track.class));
+                    Track newTrack = new Track(
+                            it.child("spotifyId").getValue(String.class),
+                            it.child("name").getValue(String.class),
+                            it.child("imageUrl").getValue(String.class)
+                    );
+                    result.add(newTrack);
                 }
+                SummaryActivity.foundTracks.set(true);
             }
 
             @Override
